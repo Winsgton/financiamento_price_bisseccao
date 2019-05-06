2e632aa08f49536c2c8fae6fe838aa09cdefb7e5
Winsgton Oliveira Gonçalves <32884954+Winsgton@users.noreply.github.com>
2019-05-06 08:12:20 -0300

    Update bisseccao.m

--- bisseccao.m
+++ bisseccao.m
@@ -1,30 +1,6 @@
 function [xm]=bisseccao(SD,TAXA,p) 
 
-  a=0;  
-  b=SD;
-  aux = SD;
-  for i=1:50
-    
-    xm = (a+b)/2;
-	
-	SD=aux;
-    for i=1:p
-      SD=func(SD, TAXA, xm);
-    endfor
-	
-	erro=abs(SD);
-	
-	if(erro<0.001)
-      return;  %retornar xm
-    endif
-	
-	if(SD > 0)
-      % novo invertalo de a [xm, SD]
-      a=xm;
-    else
-    %novo intervalo de SD [xm, SD]
-      b=xm;
-    endif
+ 
   
   endfor
 
