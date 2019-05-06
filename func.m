74cb314d63f5a404a8f5e51f62755af41e992e5d
Winsgton Oliveira Gonçalves <32884954+Winsgton@users.noreply.github.com>
2019-05-06 08:12:45 -0300

    Update func.m

--- func.m
+++ func.m
@@ -1,5 +1,3 @@
 function SD=func(SD, TAXA, xm)
-	JUROS = TAXA*SD;
-	SD = SD + JUROS - xm;
-	%xm é o suposto valor da parcela que anularia o saldo devedor, este calculado pela bisseccao
+	
 endfunction
